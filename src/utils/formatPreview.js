99b64b4c9d37f0c3e6a0171d3559aed3a02ff22a
Scott Sheppard <scott.sheppard@hey.com>
2024-12-02 13:38:26 -0800

    fixes bold rendring in backlinks

--- src/utils/formatPreview.js
+++ src/utils/formatPreview.js
@@ -1,7 +1,14 @@
-
 // src/utils/formatPreview.js
 export function formatPreview(content) {
-  return content
-    .replace(/\[\[(\d+)\s([^\]]+)\]\]/g, '**$2**')
-    .substring(0, 100) + '...';
+  // First replace links with bold text
+  const withBoldLinks = content.replace(/\[\[(\d+)\s([^\]]+)\]\]/g, '**$2**');
+  
+  // Then truncate
+  const truncated = withBoldLinks.substring(0, 100) + '...';
+  
+  // Only complete bold syntax if there's an odd number of ** sequences
+  const starCount = (truncated.match(/\*\*/g) || []).length;
+  return starCount % 2 === 1 
+    ? truncated.replace(/\.{3}$/, '**...')
+    : truncated;
 }
