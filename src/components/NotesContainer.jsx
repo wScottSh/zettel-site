1bb871ad4f0f754692bb6c02edd7bd15f3e35366
Scott Sheppard <scott.sheppard@hey.com>
2024-12-02 13:05:30 -0800

    scrolls to end of list when adding new notes

--- src/components/NotesContainer.jsx
+++ src/components/NotesContainer.jsx
@@ -1,17 +1,27 @@
 // src/components/NotesContainer.jsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Note from './Note';
 import { NOTES_DATABASE } from '../data/notesDatabase';
 
 function NotesContainer({ noteIds }) {
+  const containerRef = useRef(null);
   const displayedNotes = noteIds
     .map(id => NOTES_DATABASE[id])
     .filter(note => note !== undefined);
 
+  useEffect(() => {
+    if (containerRef.current && displayedNotes.length > 0) {
+      containerRef.current.scrollTo({
+        left: containerRef.current.scrollWidth,
+        behavior: 'smooth'
+      });
+    }
+  }, [displayedNotes.length]);
+
   return (
-    <section className="notes-container">
-      {displayedNotes.map(note => (
-        <Note key={note.id} {...note} />
+    <section className="notes-container" ref={containerRef}>
+      {displayedNotes.map((note, index) => (
+        <Note key={`${note.id}-${index}`} {...note} />
       ))}
     </section>
   );
